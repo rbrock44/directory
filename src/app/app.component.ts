e3addc7564c3583851eaa7bf835a55b4f76640dd
rbrock44 <rbrock444@gmail.com>
2025-08-01 15:54:56 -0500

    add project github link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,15 @@ import {CommonModule, Location} from "@angular/common";
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 interface Project {
-  name: string,
-  description: string,
-  link?: string
+  name: string;
+  description: string;
+  link?: string;
+  github?: string;
 }
 
 interface Company {
-  name: string,
-  projects: Project[]
+  name: string;
+  projects: Project[];
 }
 
 @Component({
@@ -41,11 +42,13 @@ export class AppComponent implements OnInit {
     {
       name: 'Home Page',
       link: this.createLink('home-page'),
+      github: this.createGithub('home-page'),
       description: 'Quick access to information Ryan wants'
     },
     {
       name: 'Woodworking Projects',
       link: this.createLink('woodworking-projects'),
+      github: this.createGithub('woodworking-projects'),
       description: 'Shows off the woodworking projects'
     },
     // {
@@ -56,56 +59,67 @@ export class AppComponent implements OnInit {
     {
       name: 'Family Recipes',
       link: this.createLink('family-recipes'),
+      github: this.createGithub('family-recipes'),
       description: `All the family\'s recipes (and more)`
     },
     {
       name: 'Add Recipe',
       link: this.createLink('add-recipe'),
+      github: this.createGithub('add-recipe'),
       description: 'A user friendly place to turn a recipe into JSON format for the family recipe website'
     },
     {
       name: 'Connect 4 [WIP]',
       link: this.createLink('connect-4'),
+      github: this.createGithub('connect-4'),
       description: 'Work in Progress -> Play Connect 4 against various AI difficulties to have some fun and increase skill'
     },
     {
       name: 'Compare Achievements [WIP]',
       link: this.createLink('compare-achievements'),
+      github: this.createGithub('compare-achievements'),
       description: 'Work in Progress -> Compare achievements across multiple platforms and users with ease'
     },
     {
       name: 'Flash Cards',
       link: this.createLink('flash-cards'),
+      github: this.createGithub('flash-cards'),
       description: 'Flash cards to help study'
     },
     {
       name: 'Cleaning Schedule',
       link: this.createLink('cleaning-schedule'),
+      github: this.createGithub('cleaning-schedule'),
       description: 'Used to help keep track of people\'s cleaning schedule'
     },
     {
       name: 'Scorekeeping By Rounds',
       link: this.createLink('scorekeeping-by-rounds'),
+      github: this.createGithub('scorekeeping-by-rounds'),
       description: 'Helps manage a game where teams/players keep score over multiple rounds'
     },
     {
       name: '1705 David',
       link: this.createLink('1705-David'),
+      github: this.createGithub('1705-David'),
       description: 'A place to showcase a rent house'
     },
     {
       name: 'Auction Advisor',
       link: this.createLink('auction-advisor'),
+      github: this.createGithub('auction-advisor'),
       description: 'Helps manage an in person silent auction'
     },
     {
       name: 'Email Link Generator',
       link: this.createLink('email-link-generator'),
+      github: this.createGithub('email-link-generator'),
       description: 'Helps create email links with parameters (fields)'
     },
     {
       name: 'Lost?',
       link: this.createLink('lost'),
+      github: this.createGithub('lost'),
       description: 'A simple lost/redirection webpage for all unknown routes on my domain'
     }
   ];
@@ -215,6 +229,10 @@ export class AppComponent implements OnInit {
 
   createLink(name: string): string {
     return `https://${name}.ryan-brock.com/`
+  }
+
+  createGithub(name: string): string {
+    return `https://github.com/rbrock44/${name}`
   }
 
   toggleDescription(app: any, event: Event) {
